eaca0c35852896284364aaf437779598418ebd20
shaheedismail <shaheed.ismail@gmail.com>
2022-08-26 22:54:53 +0100

    Scrapes running

--- jsondataset.py
+++ jsondataset.py
@@ -38,7 +38,7 @@ if __name__ == '__main__':
             with gzip.open(file) as f:
                 for i, l in enumerate(f):
                     dataset.append(json.loads(l.strip()))
-                    if i < 200000: continue
+                    if i < 10000: continue
                     else: break
     df = pd.DataFrame.from_dict(dataset)
     df.to_csv('Datasetjson.csv', index=False)
